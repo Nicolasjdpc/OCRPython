9bff397425feb6d485ac7fa7a269aa6b7464e0af
Nicolasjdpc <nicolasjdpc@gmail.com>
2021-10-16 14:08:00 -0500

    Update ocrTest.py
    Reconocimiento de fechas de nacimiento

--- ocrTest.py
+++ ocrTest.py
@@ -4,68 +4,71 @@ import datefinder
 
 pytesseract.pytesseract.tesseract_cmd= r'C:\Program Files\Tesseract-OCR\tesseract'
 
-image = cv2.imread('img.jpg')
 
+image = cv2.imread('img.jpg')
 
 text = pytesseract.image_to_string(image,lang='spa')
 
-print('Texto: ',text)
 
-print('posicion: ',text.find('FECHA DE NA'))
-
-y= text.find('FECHA DE NA') +12
-cadena=text[y:(y+20)]
-print('fech: ',cadena)
-
-
-def month_string_to_number(string):
-    m = {
-        'ene': 1,
-        'feb': 2,
-        'mar': 3,
-        'abr':4,
-         'may':5,
-         'jun':6,
-         'jul':7,
-         'ago':8,
-         'sep':9,
-         'oct':10,
-         'nov':11,
-         'dic':12
-        }
-    s = string.strip()[:3].lower()
+def month_string_to_number(SS):
+    #año no ingresa aqui
+    valor=SS.strip(' ')
+    out ="-"
+    m = [ {'mes':'ENE', 'num':'-1-'}, {'mes':'FEB', 'num':'-2-'},         
+          {'mes':'MAR', 'num':'-3-'}, {'mes':'ABR', 'num':'-4-'},          
+          {'mes':'MAY', 'num':'-5-'}, {'mes':'JUN', 'num':'-6-'},
+          {'mes':'JUL', 'num':'-7-'},{'mes':'AGO', 'num':'-8-'},
+          {'mes':'SEP', 'num':'-9-'},{'mes':'OCT', 'num':'-10-'},
+          {'mes':'NOV', 'num':'-11-'},{'mes':'DIC', 'num':'-12-'},       
+        ]   
+    
+    for dic in m:
+        if dic.get('mes')==valor:
+            return dic.get('num')      
+    return SS
     try:
-        out = m[s]
-        return out
+        return '|'
     except:
-        return 0
+        return '|'
 
-matches = list(datefinder.find_dates(cadena))
-
-if len(matches) > 0:
-    # date returned will be a datetime.datetime object. here we are only using the first match.
-    date = matches[0]
-    print (date)
-else:
-    for indice in range(len(cadena)):
-        caracter = cadena[indice]
-        print(caracter)
-        palabra=""
-        if caracter != '':
+def recorreCadena(txt):
+    txt=txt.replace('\n', ' ').replace('\r', '')
+    cadenaReparada=""
+    palabra=''
+    for indice in range(len(txt)):
+        caracter= txt[indice]
+        if caracter !=' ':
             palabra=palabra+caracter            
-            print('palabra',palabra)
         else:
-            numero=0
-            numero=month_string_to_number(palabra)
-            palabra=""
-            if numero>0:
-                cadena.replace(palabra,numero)
-print ('fecha cambio',cadena)
+           cadenaReparada= cadenaReparada+" "+month_string_to_number(palabra)           
+           palabra=" "
+    return cadenaReparada
+
+#print('Texto: ',text)
+
+def devolverFecha(cadena):
+    matches = list(datefinder.find_dates(cadena))
+    if len(matches) > 0:
+        date = matches[0]
+        return date
+    else:
+        return 0
+y= text.find('ECHA DE NA') 
+cadena=text[y:(y+30)]
+#print('cadena',cadena)
+#print('fin cadena')
+
+fecha=devolverFecha(cadena)
+
+if fecha!=0:
+  print (fecha)  
+else:    
+    fecha=devolverFecha(recorreCadena(cadena))
+    print(fecha)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
 
 
 
